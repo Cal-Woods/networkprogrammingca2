715099d4e0ac2ed77e91524e58e401cdd127c1ca
Cal Woods <D00270955@student.dkit.ie>
2026-03-25 11:02:15 +0000

    Feat: Added service class TcpServer that implements interfaces.Server interface.
    Will act as service for tcp server operations.
    
    Server interface requires TcpServer to contain two methods: void startServer() for opening connection and void stopServer() to close connection.

---
+++ src/main/java/services/TcpServer.java
@@ -0,0 +1,92 @@
+package services;
+
+import interfaces.Server;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+/**
+ * Provides TCP server object with built-in TCP actions as a service.
+ */
+@Slf4j
+public class TcpServer implements Server {
+    int port;
+    int backLog = -1;
+    @NonNull InetAddress address;
+    @NonNull ServerSocket serverSocket;
+    Socket clientSocket;
+
+    /**
+     * Setup instance with given port to use in binding to current host ip address.
+     * @param port Given port, between 0 - 65535
+     */
+    public TcpServer(int port) throws IOException {
+        setPort(port);
+        this.address = InetAddress.getLocalHost();
+
+        this.serverSocket = new ServerSocket(port);
+    }
+
+    /**
+     * Setup instance with given port, address and backlog values.
+     * @param port Given port, between 0 - 65535
+     * @param backLog given backlog as requested max number of connections
+     * @param address Given {@link InetAddress} object as ip address, cannot be null
+     */
+    public TcpServer(int port, int backLog, InetAddress address) throws IOException {
+        setPort(port);
+        setBackLog(backLog);
+        this.address = address;
+
+        this.serverSocket = new ServerSocket(port, backLog, address);
+    }
+
+    /**
+     * Starts TCP server by listening for connections.
+     *
+     * @implNote Method blocks until connection is made.
+     */
+    public void startServer() {
+        try {
+            this.clientSocket = serverSocket.accept();
+        }
+        catch (IOException e) {
+            log.error("Could not accept connection and create clientSocket! {}", e.toString());
+            System.err.println("Could not start server!" + e.toString());
+        }
+    }
+
+    /**
+     * Stops TCP server and closes connections
+     */
+    public void stopServer() {
+        try {
+            this.clientSocket.close();
+        }
+        catch (IOException e) {
+            log.error("Could not close connection and close clientSocket! {}", e.toString());
+            System.err.println("Could not close server!" + e.toString());
+        }
+    }
+    private void setPort(int port) {
+        if (port < 0 || port > 65535) {
+            log.error("Could not set port as given port was out of range! {}", port);
+            throw new IllegalArgumentException("Given port must be between 0 and 65535!");
+        }
+
+        this.port = port;
+    }
+
+    private void setBackLog(int backLog) {
+        if (backLog < 0) {
+            log.error("Could not set back log as given back log cannot be negative! {}", backLog);
+            throw new IllegalArgumentException("Given back log must not be < 0!");
+        }
+
+        this.backLog = backLog;
+    }
+}
