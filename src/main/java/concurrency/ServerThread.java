8a81f1a9ae4a661723aff412e9349b9d629310cb
Cal Woods <D00270955@student.dkit.ie>
2026-03-27 15:57:33 +0000

    Feat: Added class ServerThread that implements java.lang.Runnable interface for using the public void run() method that Java Thread objects call.
    Logs info using the org.Slf4j.Logger implementation.

---
+++ src/main/java/concurrency/ServerThread.java
@@ -0,0 +1,24 @@
+package concurrency;
+
+import interfaces.Server;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * Runs a server interface implementation for multithreading capabilities.
+ *
+ * @author Cal Woods
+ */
+@Slf4j
+public class ServerThread implements Runnable {
+    Server server;
+
+    public ServerThread(Server server) {
+        this.server = server;
+    }
+
+    @Override
+    public void run() {
+        log.info("Starting new server on separate thread!");
+        this.server.startServer();
+    }
+}
