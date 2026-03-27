2b352f256aa95043ceac06a8e3f18ebbeded51c9
Cal Woods <D00270955@student.dkit.ie>
2026-03-27 16:23:03 +0000

    Feat: Added class TcpMultiThreadServer that will act as main program for this TCP server with multiple threads for client connections.
    Each connected client connection will run on a separate thread for concurrency.
    
    Concurrency not implemented yet.
    
    As part of project specification.

---
+++ src/main/java/TcpMultiThreadServer.java
@@ -0,0 +1,42 @@
+import concurrency.ServerThread;
+import interfaces.Server;
+import lombok.extern.slf4j.Slf4j;
+import services.TcpServer;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Scanner;
+
+@Slf4j
+public class TcpMultiThreadServer {
+    //Setup
+    static int SERVER_PORT = 5555;
+    static InetAddress SERVER_ADDRESS;
+    //Set max number of connections
+    static int BACK_LOG = 5;
+    static Runnable serverThread;
+    static TcpServer tcpServer;
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        Server tcpServer = new TcpServer(SERVER_PORT);
+        serverThread = new ServerThread(tcpServer);
+        Thread[] threads = new Thread[BACK_LOG];
+        //Setup threads Thread array
+        for (int i = 0; i < BACK_LOG; i++) {
+            threads[i] = new Thread(serverThread);
+            //Start threads[i]
+            threads[i].start();
+        }
+    }
+
+    /**
+     * Gets user input via a local {@link Scanner} object sc.
+     * @param prompt Given prfompt to display
+     * @return Obtained {@link String} from {@link Scanner}
+     */
+    private static String getInput(String prompt) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(prompt);
+        return sc.nextLine();
+    }
+}
