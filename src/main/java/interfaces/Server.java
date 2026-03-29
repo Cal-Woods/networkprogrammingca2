504a81a4ebe09ae3256f2462899e23599c3d364b
Cal Woods <D00270955@student.dkit.ie>
2026-03-29 14:34:07 +0100

    Docs: Added javadocs to all method signatures.

--- src/main/java/interfaces/Server.java
+++ src/main/java/interfaces/Server.java
@@ -1,7 +1,18 @@
 package interfaces;
 
+import model.Email;
+
+import java.time.LocalDateTime;
+
 public interface Server {
+    /**
+     * Starts server on configured port.
+     */
     void startServer();
+
+    /**
+     * Stops server.
+     */
     void stopServer();
 
     /**
