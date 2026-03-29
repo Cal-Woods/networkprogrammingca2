eec56b3aefe5af4837d31ef2abdad549c3ed72ff
Cal Woods <D00270955@student.dkit.ie>
2026-03-29 14:47:56 +0100

    Refactor: Removed instance Server implementation and all related calls.

--- src/main/java/concurrency/ServerThread.java
+++ src/main/java/concurrency/ServerThread.java
@@ -1,6 +1,5 @@
 package concurrency;
 
-import interfaces.Server;
 import lombok.extern.slf4j.Slf4j;
 
 /**
@@ -10,12 +9,6 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class ServerThread implements Runnable {
-    Server server;
-
-    public ServerThread(Server server) {
-        this.server = server;
-    }
-
     @Override
     public void run() {
 
