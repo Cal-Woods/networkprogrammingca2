d042356d51c1504de93eceaf0455b7812d81b952
Cal Woods <D00270955@student.dkit.ie>
2026-03-29 14:33:32 +0100

    Feat: Added methods boolean sendEmail(String recipient, String subject, String body, LocalDateTime timestamp) and Email receiveEmail().
    Javadocs added to both method signatures.

--- src/main/java/interfaces/Server.java
+++ src/main/java/interfaces/Server.java
@@ -3,4 +3,15 @@ package interfaces;
 public interface Server {
     void startServer();
     void stopServer();
+
+    /**
+     * Sends an email over network connection.
+     * @return True if email sent
+     */
+    boolean sendEmail(String recipient, String subject, String body, LocalDateTime timestamp);
+
+    /**
+     * Receives email from client.
+     */
+    Email receiveEmail();
 }
