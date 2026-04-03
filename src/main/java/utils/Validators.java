e9211246f9d46a580c3477d16f2ec015627ec486
Cal Woods <D00270955@student.dkit.ie>
2026-04-03 15:18:58 +0100

    Feat: Added class Validators that contains global validation methods: public static void validateEmail(String email) that checks if a given String is a valid email, and public String validateStringData(String inputData) that does basic String checking for null or is blank.

---
+++ src/main/java/utils/Validators.java
@@ -0,0 +1,34 @@
+package utils;
+
+import exceptions.InvalidEmailFormatException;
+
+/**
+ * Contains global validator methods.
+ */
+public class Validators {
+    /**
+     * Global validate email method that checks given email address for an email pattern.
+     * @param email Given email address
+     * @throws InvalidEmailFormatException If email address fails validation
+     */
+    public static void validateEmail(String email) {
+        String input = validateStringData(email);
+
+        if(!input.matches("^.+@.+[.][a-zA-Z]{2,}")) {
+            throw new InvalidEmailFormatException("Given email data is an invalid address!");
+        }
+    }
+
+    public static String validateStringData(String inputData) {
+        if(inputData == null) {
+            throw new InvalidEmailFormatException("Could not validate email data as given input data was null!");
+        }
+        if(inputData.isBlank()) {
+            throw new InvalidEmailFormatException("Could not validate given input data as it was empty!");
+        }
+
+        String trimmedData = inputData.trim();
+
+        return trimmedData;
+    }
+}
