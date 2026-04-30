9388965362997cdadfbc00db3a1456bc81b8a759
Cal Woods <D00270955@student.dkit.ie>
2026-04-30 22:16:23 +0100

    Feat: Added new version of public String authenticate() to AuthService that handles transmitted user credentials for login and returns a token based on given ip address.
    Ip address should be gotten from client ip address.

--- src/main/java/services/AuthService.java
+++ src/main/java/services/AuthService.java
@@ -2,15 +2,17 @@ package services;
 
 import com.password4j.Argon2Function;
 import com.password4j.Hash;
+import com.password4j.HashingFunction;
 import com.password4j.Password;
 import com.password4j.types.Argon2;
 import exceptions.InvalidEmailFormatException;
 import lombok.extern.slf4j.Slf4j;
 import model.RegisterModel;
+import utils.Validators;
 
-import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Authentication services for server with Argon2 hashing.
@@ -20,7 +22,9 @@ import java.util.UUID;
  */
 @Slf4j
 public class AuthService {
-    private final Map<String, String> credentials = new HashMap<>();
+    private final HashingFunction hasher = Argon2Function.getInstance(16, 4, 2, 60, Argon2.ID);
+    private final Map<String, String> credentials = new ConcurrentHashMap<>();
+    private final Map<String, String> tokens = new ConcurrentHashMap<>();
     private final String pepper;
 
     public AuthService() {
@@ -30,11 +34,44 @@ public class AuthService {
         this.pepper = pepper + "-" + UUID.randomUUID().toString();
     }
 
-    public boolean authenticate(String username, String password) {
-        return password.equals(credentials.get(username));
+    /**
+     * Authenticates a user with given login credentials.
+     * @param username Given username
+     * @param password Given password
+     * @throws InvalidEmailFormatException If given username is not a valid email address
+     * @return A {@link String} token representing login session
+     */
+    public String authenticate(String username, String password, String ipAddress) throws InvalidEmailFormatException {
+        String validatedIp = Validators.validateStringData(ipAddress);
+        String validatedUsername = Validators.validateStringData(username);
+        Validators.validateEmail(validatedUsername);
+
+        if (!credentials.containsKey(validatedUsername)) {
+            return null;
+        }
+
+        String validatedPassword = Validators.validateStringData(password);
+        if (validatedPassword.length() < 8) {
+            return null;
+        }
+
+        if (!hasher.check(validatedPassword, credentials.get(validatedUsername))) {
+            return null;
+        }
+
+        String token = UUID.fromString(validatedIp).toString();
+        tokens.put(token, validatedUsername);
+
+        return token;
     }
 
-
+    /**
+     * Validates register data as a {@link RegisterModel} object.
+     * @param registerModel Given {@link RegisterModel} object
+     * @return True if registration successful
+     * @throws IllegalArgumentException If given data was invalid
+     * @throws InvalidEmailFormatException If given email was invalid
+     */
     public boolean register(RegisterModel registerModel) throws IllegalArgumentException, InvalidEmailFormatException {
         //Call registerModel instance validation methods
         try {
@@ -49,7 +86,6 @@ public class AuthService {
             }
 
             //Hash password
-            Argon2Function hasher = Argon2Function.getInstance(16, 4, 2, 60, Argon2.ID);
             Hash hashed = Password.hash(registerModel.getPassword()).addRandomSalt(12).addPepper(pepper).with(hasher);
 
             //Put new credentials
@@ -61,8 +97,8 @@ public class AuthService {
             throw new IllegalArgumentException("Invalid data received from RegisterModel!");
         }
         catch(InvalidEmailFormatException e) {
-            log.error("Cannot perform register action as invalid data received from RegisterModel!");
-            throw new IllegalArgumentException("Invalid data received from RegisterModel!");
+            log.error("Cannot perform register action as invalid email received from RegisterModel!");
+            throw new IllegalArgumentException("Invalid email received from RegisterModel!");
         }
     }
 }
